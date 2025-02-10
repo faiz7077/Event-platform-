6152f47dea80855c79eec4cb5d3638665490f389
Faizan Shaikh <faizanshaikh008007@gmail.com>
2025-02-10 20:01:43 +0530

    Added clerk middleware

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
-
+import Footer from "@/components/shared/Footer"
+import Header from "@/components/shared/Header"
 
 export default function RootLayout({
   children,
@@ -7,9 +8,9 @@ export default function RootLayout({
 }) {
   return (
     <div className="flex h-screen flex-col">
-
+      <Header />
       <main className="flex-1">{children}</main>
-
+      <Footer />
     </div>
   )
 }
