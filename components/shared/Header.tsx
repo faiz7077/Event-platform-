312c598bac7ffd17976d531febb0641b29389b15
Faizan Shaikh <faizanshaikh008007@gmail.com>
2025-02-10 20:55:54 +0530

    Auth completed

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,7 @@
-
+import { SignedOut } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 const Header = () => {
 return (
     <header className='w-full bg-bottom'>
@@ -8,7 +9,15 @@ return (
             <Link href="/" className="w-36">
             <Image src="/assets/images/logo.svg" width={128} height={38} alt='Evently Logo'/>
             </Link>
-            <div className="flex w-32  justify-end gap-3"></div>
+            <div className="flex w-32  justify-end gap-3">
+                <SignedOut>
+                    <Button asChild className="rounded-full" size="lg">
+                        <Link href="/sign-in">
+                        Login
+                        </Link>
+                    </Button>
+                </SignedOut>
+            </div>
         </div>
     </header>
   )
